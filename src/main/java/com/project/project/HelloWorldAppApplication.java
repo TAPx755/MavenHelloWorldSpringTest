f715f553533db31e022b25201e1843a55d23460b
TAPx755 <patzangerl@tsn.at>
2018-12-17 14:35:01 +0100

    2nd Test

--- src/main/java/com/project/project/HelloWorldAppApplication.java
+++ src/main/java/com/project/project/HelloWorldAppApplication.java
@@ -14,6 +14,7 @@ public class HelloWorldAppApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 }
 
