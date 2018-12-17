7caad881d550206a7eee517fc5b303c73916b664
paddy <paddy.@paddys-mac.local>
2018-12-17 14:33:20 +0100

    Initial commit

---
+++ src/main/java/com/project/project/HelloWorldAppApplication.java
@@ -0,0 +1,19 @@
+package com.project.project;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldAppApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldAppApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World");
+	}
+}
+
